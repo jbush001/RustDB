3630dacf40f81fa5076ad0ed3fc8bafe546af9fc
Jeff Bush <jeffbush001@gmail.com>
2026-05-10 17:46:46 -0700

    Update split_node semantics
    When an internal node is split, the middle key is pushed up into the parent. split_node now always returns the middle key and will remove it for non-leaf nodes (for leaf nodes, the value is left in)

--- src/btree.rs
+++ src/btree.rs
@@ -14,39 +14,57 @@
 //   limitations under the License.
 //
 
+// This is a B+ tree implementation. Keys are only stored in the leaf nodes.
+
 use std::cmp::Ordering;
 use crate::util::*;
 
-// Btree node format
-// [0..1] type_flags   u16
-// [2..3] entries_start  u16
-// [4..5] num_entries    u16
-// [6..13] prev_sib    u64
-// [14..21] next_sib   u64
-// index: slot_offs[i] u16
-// free space
-// entries.
-//   each entry:
-//   [0..1] key length
-//   [2..key_len] key
-//   [key_len..key_len + 8] value
+// Node format
+// Bytes  |  Type  | Description
+// -------|--------|----------------------
+// 0..1   |  u16   | Flags
+// 2..3   |  u16   | Start of entry storage
+// 4..5   |  u16   | Num entries
+// 6..13  |  u64   | Next sibling leaf, for in-order traversal
+// 14..21 |  u64   | Prev sibling leaf, same as above
+// 22..29 |  u64   | Right child. Value for nodes greater than max key
+// 30..   |  u16   | Index (see below)
+//        |        | Free space
+// N..    |        | Entry storage
+//
+// Each entry is:
+//  key_length: u16
+//  key: [u8; key_length]
+//  value: u64
 //
 // The index contains offsets to each entry in the node. The index entries
 // are always sorted in lexigraphical order, but the entries themselves
 // do not have to be in sorted. The entries are, however always contiguous.
+// As entries are added, they grow downward.
 // The 'entries_start' field contains the address of the lowest entry.
 // Each entry is a key/value pair, where the key is a variable length
 // field and the value is a 64-bit integer.
+// the 'right_child' field for internal nodes represents the child that
+// is greater than all the keys. Since each entry in the page splits
+// the keys, you end up with one extra pointer. Rather then using a
+// dummy node, it is just stored here.
 //
 
 const ENTRY_START_FIELD_OFFS: usize = 2;
 const NUM_ENTRIES_FIELD_OFFS: usize = 4;
-const INDEX_OFFS: usize = 22;
+const RIGHT_CHILD_FIELD_OFFS: usize = 22;
+const INDEX_OFFS: usize = 30;
+
+const FLAG_LEAF: u16 = 1;
 
 // Create an empty node
 fn init_node(node: &mut [u8]) {
     node.fill(0);
     set_u16(node, ENTRY_START_FIELD_OFFS, node.len() as u16);
+}
+
+fn is_leaf(node: &[u8]) -> bool {
+    (get_u16(node, 0) & FLAG_LEAF) != 0
 }
 
 fn get_num_entries(node: &[u8]) -> usize {
@@ -75,7 +93,12 @@ fn get_entry_key(node: &[u8], rec_num: usize) -> &[u8] {
 }
 
 fn get_entry_value(node: &[u8], rec_num: usize) -> u64 {
-    assert!(rec_num < get_num_entries(node));
+    let num_entries = get_num_entries(node);
+    assert!(rec_num <= num_entries);
+
+    if rec_num == num_entries {
+        return get_u64(node, RIGHT_CHILD_FIELD_OFFS);
+    }
 
     let entry_offs = get_entry_offs(node, rec_num);
     let key_len = get_u16(node, entry_offs) as usize;
@@ -152,7 +175,8 @@ fn append_entry(node: &mut [u8], key: &[u8], value: u64) -> usize {
     entry_length
 }
 
-fn split_node(orig: &[u8], out1: &mut [u8], out2: &mut [u8]) {
+// Returns the separator key.
+fn split_node(orig: &[u8], out1: &mut [u8], out2: &mut [u8]) -> Vec<u8> {
     init_node(out1);
     init_node(out2);
 
@@ -170,12 +194,25 @@ fn split_node(orig: &[u8], out1: &mut [u8], out2: &mut [u8]) {
         orig_index += 1;
     }
 
+    let separator = get_entry_key(orig, orig_index).to_vec();
+
+    if !is_leaf(orig) {
+        // Remove the separator key, which will go into the parent. Save its value
+        // into the right child.
+        set_u64(out1, RIGHT_CHILD_FIELD_OFFS, get_entry_value(orig, orig_index));
+        orig_index += 1;
+    }
+
     // Copy into out2
     while orig_index < orig_entries {
         append_entry(out2, get_entry_key(orig, orig_index),
             get_entry_value(orig, orig_index));
         orig_index += 1;
     }
+
+    set_u64(out2, RIGHT_CHILD_FIELD_OFFS, get_u64(orig, RIGHT_CHILD_FIELD_OFFS));
+
+    separator
 }
 
 fn delete_entry(node: &mut [u8], index: usize) {
@@ -226,7 +263,7 @@ mod tests {
         }
 
         for i in 0..num_entries {
-            let rec_offs = get_u16(node, 22 + i * 2) as usize;
+            let rec_offs = get_u16(node, super::INDEX_OFFS + i * 2) as usize;
             assert_lt!(rec_offs, node.len());
             sorted_rec_offs.push(rec_offs);
         }
@@ -251,7 +288,7 @@ mod tests {
         // Ensure the keys are in order
         let mut last_key: &[u8] = &[0];
         for i in 0..num_entries {
-            let entry_offs = get_u16(node, 22 + i * 2) as usize;
+            let entry_offs = get_u16(node, super::INDEX_OFFS + i * 2) as usize;
             let key_len = get_u16(&node, entry_offs) as usize;
             let key_start = entry_offs + 2;
 
@@ -298,7 +335,7 @@ mod tests {
         super::append_entry(&mut node, "a".as_bytes(), 0);
         super::append_entry(&mut node, "z".as_bytes(), 0);
 
-        let rec1_offs = get_u16(&node, 24) as usize; // second entry
+        let rec1_offs = get_u16(&node, super::INDEX_OFFS + 2) as usize; // second entry
         node[rec1_offs] += 1;
 
         sanity_check_node(&node);
@@ -308,6 +345,7 @@ mod tests {
     fn test_get_key_val() {
         let mut node: [u8; 4096] = [0; 4096];
         super::init_node(&mut node);
+        set_u64(&mut node, super::RIGHT_CHILD_FIELD_OFFS, 0x1111222233334444);
 
         super::append_entry(&mut node, "foobar".as_bytes(), 0x12345678abcdef);
         super::append_entry(&mut node, "zzzz".as_bytes(), 0xfedbca87654321);
@@ -321,6 +359,9 @@ mod tests {
         let key_bytes1 = super::get_entry_key(&node, 1);
         assert_eq!(key_bytes1, "zzzz".as_bytes());
         assert_eq!(super::get_entry_value(&node, 1), 0xfedbca87654321);
+
+        // Asking for the last entry should return the right child value.
+        assert_eq!(super::get_entry_value(&node, 2), 0x1111222233334444);
     }
 
     #[test]
@@ -364,9 +405,9 @@ mod tests {
     fn test_get_free_space() {
         let mut node: [u8; 4096] = [0; 4096];
         super::init_node(&mut node);
-        assert_eq!(super::get_node_free_space(&node), 4096 - 22);
+        assert_eq!(super::get_node_free_space(&node), 4096 - super::INDEX_OFFS);
         super::insert_entry(&mut node, "aardvark".as_bytes(), 1000);
-        assert_eq!(super::get_node_free_space(&node), 4096 - 42);
+        assert_eq!(super::get_node_free_space(&node), 4096 - super::INDEX_OFFS - 20);
     }
 
     #[test]
@@ -399,7 +440,7 @@ mod tests {
     }
 
     #[test]
-    fn test_split_node() {
+    fn test_split_interior_node() {
         let mut node1: [u8; 4096] = [0; 4096];
         let mut node2: [u8; 4096] = [0; 4096];
         let mut node3: [u8; 4096] = [0; 4096];
@@ -413,9 +454,59 @@ mod tests {
 
         sanity_check_node(&node1);
 
-        super::split_node(&node1, &mut node2, &mut node3);
+        let separator_key = super::split_node(&node1, &mut node2, &mut node3);
         sanity_check_node(&node2);
         sanity_check_node(&node3);
+
+        let orig_sep_index = super::get_num_entries(&node2);
+        assert_eq!(&separator_key, &super::get_entry_key(&node1, orig_sep_index));
+        assert_eq!(get_u64(&node2, super::RIGHT_CHILD_FIELD_OFFS),
+            super::get_entry_value(&node1, orig_sep_index));
+        assert_eq!(get_u64(&node3, super::RIGHT_CHILD_FIELD_OFFS),
+            get_u64(&node1, super::RIGHT_CHILD_FIELD_OFFS));
+
+        // Ensure all entries are present and in order
+        let node2_recs = super::get_num_entries(&node2);
+        assert_eq!(super::get_num_entries(&node1) - 1,
+            node2_recs + super::get_num_entries(&node3));
+        assert_lt!(node2_recs, PAGE1_ENTRIES * 2 / 3);
+        for i in 0..super::get_num_entries(&node1) {
+            if i == node2_recs {
+                continue; // ignore splitter
+            }
+
+            let orig_entry = super::get_entry_key(&node1, i);
+            if i > node2_recs {
+                assert_eq!(orig_entry, super::get_entry_key(&node3, i - node2_recs - 1));
+            } else {
+                assert_eq!(orig_entry, super::get_entry_key(&node2, i));
+            }
+        }
+    }
+
+    #[test]
+    fn test_split_leaf_node() {
+        let mut node1: [u8; 4096] = [0; 4096];
+        let mut node2: [u8; 4096] = [0; 4096];
+        let mut node3: [u8; 4096] = [0; 4096];
+
+        super::init_node(&mut node1);
+        set_u16(&mut node1, 0, super::FLAG_LEAF);
+
+        const PAGE1_ENTRIES: usize = 25;
+        for i in 0..PAGE1_ENTRIES {
+            let key = vec![b'A' + i as u8; 128];
+            super::insert_entry(&mut node1, &key, i as u64);
+        }
+
+        sanity_check_node(&node1);
+
+        let separator_key = super::split_node(&node1, &mut node2, &mut node3);
+        sanity_check_node(&node2);
+        sanity_check_node(&node3);
+
+        let orig_sep_index = super::get_num_entries(&node2);
+        assert_eq!(&separator_key, &super::get_entry_key(&node1, orig_sep_index));
 
         // Ensure all entries are present and in order
         let node2_recs = super::get_num_entries(&node2);
@@ -485,5 +576,14 @@ mod tests {
         let mut node: [u8; 4096] = [0; 4096];
         super::init_node(&mut node);
         super::delete_entry(&mut node, 0);
+    }
+
+    #[test]
+    fn test_leaf_flag() {
+        let mut node: [u8; 4096] = [0; 4096];
+        super::init_node(&mut node);
+        assert!(!super::is_leaf(&node));
+        node[0] = 1;
+        assert!(super::is_leaf(&node));
     }
 }
