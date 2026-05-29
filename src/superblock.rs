7d46b958e7f8a28cf876169d41665c10bdb1ca84
Jeff Bush <jeffbush001@gmail.com>
2026-05-28 23:06:30 -0500

    Add magic number of superblock and fix function signature

--- src/superblock.rs
+++ src/superblock.rs
@@ -18,25 +18,56 @@ use bytemuck::{Pod, Zeroable};
 use crate::page_cache::*;
 use std::mem;
 
+const SUPERBLOCK_MAGIC: &str = "RUSTDB00";
+
 #[repr(C)]
 #[derive(Copy, Clone, Pod, Zeroable)]
 pub struct Superblock {
+    pub magic: [u8; 8],
     pub free_list_head: u64,
     pub file_size: u64
 }
 
 pub const SUPERBLOCK_FPID: FilePageId = FilePageId(0);
 
-pub fn init_superblock(page_guard: &mut PageGuardMut) {
-    let block = get_superblock_mut(page_guard);
+pub fn init_superblock(page: &mut [u8]) {
+    let block = get_superblock_mut(page);
+    block.magic.copy_from_slice(SUPERBLOCK_MAGIC.as_bytes());
     block.free_list_head = 0;
     block.file_size = 1;
 }
 
-pub fn get_superblock(page_guard: &PageGuard) -> &Superblock {
-    bytemuck::from_bytes(&page_guard[0..mem::size_of::<Superblock>()])
+pub fn check_superblock(page: &[u8]) -> Result<(), String> {
+    let block = get_superblock(page);
+    if block.magic != SUPERBLOCK_MAGIC.as_bytes() {
+        return Err("Bad Magic".to_string());
+    }
+
+    Ok(())
 }
 
-pub fn get_superblock_mut(page_guard: &mut PageGuardMut) -> &mut Superblock {
-    bytemuck::from_bytes_mut(&mut page_guard[0..mem::size_of::<Superblock>()])
+pub fn get_superblock(page: &[u8]) -> &Superblock {
+    bytemuck::from_bytes(&page[0..mem::size_of::<Superblock>()])
+}
+
+pub fn get_superblock_mut(page: &mut [u8]) -> &mut Superblock {
+    bytemuck::from_bytes_mut(&mut page[0..mem::size_of::<Superblock>()])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bad_magic() {
+        let page: [u8; 4096] = [0; 4096];
+        assert_eq!(check_superblock(&page), Err("Bad Magic".to_string()));
+    }
+
+    #[test]
+    fn test_good_magic() {
+        let mut page: [u8; 4096] = [0; 4096];
+        init_superblock(&mut page);
+        assert_eq!(check_superblock(&page), Ok(()));
+    }
 }
